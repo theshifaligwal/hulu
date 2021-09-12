678f04a7d4c93746f3d7926f1e34ee1ad38128fe
theshifaligwal <gwal.shifali2223@gmail.com>
2021-09-12 18:48:55 +0530

    Create header items

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,22 @@
+import {BadgeCheckIcon,
+CollectionIcon,
+HomeIcon,
+LightningBoltIcon,
+SearchIcon,
+UserIcon} from "@heroicons/react/outline"
+
 import Image from "next/image"
+import HeaderItem from "./HeaderItem"
 function Header() {
     return (
         <header className="">
         <div>
-        <HeaderItem />
+        <HeaderItem title="Home" Icon={HomeIcon} />
+        <HeaderItem title="Home" Icon={LightningBoltIcon} />
+        <HeaderItem title="Home" Icon={BadgeCheckIcon} />
+        <HeaderItem title="Home" Icon={CollectionIcon} />
+        <HeaderItem title="Home" Icon={SearchIcon} />
+        <HeaderItem title="Home" Icon={UserIcon} />
         </div>
             <Image className="object-contain" src='https://links.papareact.com/ua6' width={100} height={100}/>
         </header>
