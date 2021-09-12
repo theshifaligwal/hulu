d0ffe4236445d04bd5bfff970e2e8110a65614e7
theshifaligwal <gwal.shifali2223@gmail.com>
2021-09-12 18:57:26 +0530

    Complete header part

--- components/Header.js
+++ components/Header.js
@@ -9,14 +9,14 @@ import Image from "next/image"
 import HeaderItem from "./HeaderItem"
 function Header() {
     return (
-        <header className="">
-        <div>
+        <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+        <div className="flex flex-grow justify-evenly max-w-2xl">
         <HeaderItem title="Home" Icon={HomeIcon} />
-        <HeaderItem title="Home" Icon={LightningBoltIcon} />
-        <HeaderItem title="Home" Icon={BadgeCheckIcon} />
-        <HeaderItem title="Home" Icon={CollectionIcon} />
-        <HeaderItem title="Home" Icon={SearchIcon} />
-        <HeaderItem title="Home" Icon={UserIcon} />
+        <HeaderItem title="Trending" Icon={LightningBoltIcon} />
+        <HeaderItem title="Verified" Icon={BadgeCheckIcon} />
+        <HeaderItem title="Collection" Icon={CollectionIcon} />
+        <HeaderItem title="Search" Icon={SearchIcon} />
+        <HeaderItem title="Account" Icon={UserIcon} />
         </div>
             <Image className="object-contain" src='https://links.papareact.com/ua6' width={100} height={100}/>
         </header>
