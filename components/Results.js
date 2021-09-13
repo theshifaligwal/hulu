a397c3e850a33a372fe8902971cc56a52dfefb0a
theshifaligwal <gwal.shifali2223@gmail.com>
2021-09-13 09:27:35 +0530

    Create thumbnail component

---
+++ components/Results.js
@@ -0,0 +1,10 @@
+import Thumbnail from "./Thumbnail"
+export default function Results({results}) {
+    return (
+        <div>
+            {results.map(result => (
+                <Thumbnail key={result.id} result={result} />
+            ))}
+        </div>
+    )
+}
---
+++ components/Thumbnail.js
@@ -0,0 +1,7 @@
+export default function Thumbnail({reuslt}) {
+    return (
+        <div>
+            <h1>thumbnail</h1>
+        </div>
+    )
+}
