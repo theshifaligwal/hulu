75c9f5a906ba8a0bcc8e3425f7750429613733a9
Shifali Gwal <gwal.shifali2223@gmail.com>
2021-09-28 12:12:19 +0530

    Deployment Bug Fix

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,34 @@
-import {BadgeCheckIcon,
-CollectionIcon,
-HomeIcon,
-LightningBoltIcon,
-SearchIcon,
-UserIcon} from "@heroicons/react/outline"
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
-import Image from "next/image"
-import HeaderItem from "./HeaderItem"
+import Image from "next/image";
+import HeaderItem from "./HeaderItem";
 function Header() {
-    return (
-        <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
-        <div className="flex flex-grow justify-evenly max-w-2xl">
+  return (
+    <header className="flex flex-col items-center justify-between h-auto m-5 sm:flex-row">
+      <div className="flex flex-grow max-w-2xl justify-evenly">
         <HeaderItem title="Home" Icon={HomeIcon} />
         <HeaderItem title="Trending" Icon={LightningBoltIcon} />
         <HeaderItem title="Verified" Icon={BadgeCheckIcon} />
         <HeaderItem title="Collection" Icon={CollectionIcon} />
         <HeaderItem title="Search" Icon={SearchIcon} />
         <HeaderItem title="Account" Icon={UserIcon} />
-        </div>
-            <Image className="object-contain" src='https://links.papareact.com/ua6' width={100} height={100}/>
-        </header>
-    )
+      </div>
+      <Image
+        className="object-contain"
+        src="https://links.papareact.com/ua6"
+        width={100}
+        height={100}
+        alt="hulu-logo"
+      />
+    </header>
+  );
 }
 
-export default Header
+export default Header;
